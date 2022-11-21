f28a13f90dd9c330ac99f93c6707252c5284f774
ciple <jade9877@hanmail.net>
2022-11-21 19:42:34 +0900

    .

---
+++ js_study/optional_chaining/sample.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const adventurer = {
+    name: "홍길동",
+    cat : {
+        name
+    }
+}
+
+// let person = {
+//     name : "홍길동",
+//     job : "의적",
+//     // age : "18",
+// }
+
+// let name = person?.age;
+// let test1;
+// let test2 = null;
+
+// console.log(typeof test1);
+// console.log(test1);
+
+// console.log(typeof test2);
+// console.log(test2);
+
+// if(test2 == null){
+//     console.log("aaa");
+// }
+
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// let test_null = null;
+// let test_undefined;
+
+// console.log(typeof test_null);      // object
+// console.log(test_null);             // null
+
+// console.log(typeof test_undefined); // undefined
+// console.log(test_undefined);        // undefined
+
+// console.log(test_null == test_undefined);   // true
+// console.log(test_null === test_undefined);  // false
+
+// console.log(1 + test_null);         // 1
+// console.log(1 + test_undefined);    // NaN
+
+
+
+
+
