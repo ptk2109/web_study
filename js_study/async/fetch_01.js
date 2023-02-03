8396e662c3b46e5b2d296e2962a55baab572d5a7
ciple <jade9877@hanmail.net>
2023-02-03 22:27:11 +0900

    .

--- js_study/async/fetch_01.js
+++ js_study/async/fetch_01.js
@@ -1,2 +1,8 @@
-let a = 1+1
-console.log(a);
+let response = await fetch(url);
+
+if (response.ok) { // HTTP 상태 코드가 200~299일 경우
+  // 응답 몬문을 받습니다(관련 메서드는 아래에서 설명).
+  let json = await response.json();
+} else {
+  alert("HTTP-Error: " + response.status);
+}
