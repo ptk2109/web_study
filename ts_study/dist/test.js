ca302f6a2dc43bb0b66abc514f036b37632f0d37
ciple <jade9877@hanmail.net>
2023-03-05 11:49:43 +0900

    .

--- ts_study/dist/test.js
+++ ts_study/dist/test.js
@@ -1,3 +1,13 @@
 "use strict";
 const message = 'hello world';
 console.log(message);
+// 기본
+let decimal = 6; // 숫자(Number)  
+let color = "blue"; // 문자열(String)
+// 배열
+let list = [1, 2, 3]; // 배열1 
+let list2 = [1, 2, 3]; // 배열2
+// 튜플 (Tuple)
+let tuple1;
+tuple1 = ["hello", 10];
+console.log(tuple1[0].substring(1)); // 성공
--- ts_study/src/test.ts
+++ ts_study/src/test.ts
@@ -1,2 +1,16 @@
-const message: string = 'hello world';
-console.log(message);
+// const message: string = 'hello world';
+// console.log(message);
+
+
+// 기본
+let decimal: number = 6;   						// 숫자(Number)  
+let color: string = "blue";						// 문자열(String)
+
+// 배열
+let list: number[] = [1, 2, 3];				// 배열1 
+let list2: Array<number> = [1, 2, 3];	// 배열2
+
+// 튜플 (Tuple)
+let tuple1: [string, number];
+tuple1 = ["hello", 10]; 
+console.log(tuple1[0]);	 // hello
